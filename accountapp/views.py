f7ee20c15c4033b81c1498e2e01d2d09c367abb7
ljs5960 <koreastars1@naver.com>
2022-02-17 23:38:35 +0900

    Add POST & GET action on html

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,4 +6,7 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == 'POST':
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST method'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET method'})
