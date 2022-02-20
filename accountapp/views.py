d61c855937b95ef19dd8339977ed11e727efa37f
ljs5960 <koreastars1@naver.com>
2022-02-20 16:16:21 +0900

    Create changing my info page

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import render
 
 
 # Create your views here.
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 from django.urls import reverse, reverse_lazy
 
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
@@ -40,3 +41,10 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user' # Everyone contacts my page can see my info, not their info
     template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
